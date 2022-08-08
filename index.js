aa9cd43aad690e5c63976ae8c9aeea769ae284bf
Lesley Labady <labadylesley@gmail.com>
2022-08-08 16:13:47 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData (name,email){
+  return fetch('http://localhost:3000/users',{
+    method: 'POST',
+    headers:{
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body:JSON.stringify({name,email})
+  })
+  .then(res => res.json())
+  .then(usersDate => document.body.innerHTML = usersDate.id )
+  .catch(error => document.body.innerHTML = error.message)
+}
